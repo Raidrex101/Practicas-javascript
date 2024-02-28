d7f327c1af48fcd2210a151d77c88ec38b607b70
hector castro <raidrex@hotmail.com>
2024-02-27 21:39:29 -0600

    primeras practicas de javascript

---
+++ main.js
@@ -0,0 +1,49 @@
+//alert("hola")
+// variables:
+
+//let doto lo declarado con let puede cambiar 
+let nombre = "hector" //string => texto
+console.log(nombre)
+//para hacer variables con mas de 2 palabras la segundda palabra va en mayuscula en la inicial y si lleva numero este debe de ir al final.
+nombre = "Geovanny"
+console.log(nombre)
+
+//const ES5 es una constante no una variable como let asi que es un dato absoluto
+
+const numero = 40
+console.log(numero)
+
+//var uso especial y evitar su uso
+
+//var datoGlobal = "soy un dato global"
+//var datoGlobal = " spy un dato doble"
+
+//tipos de datos:
+
+//primitivos:
+// strings, number, boolean
+
+//cadenas de texto:
+//alt + 096 atajo de asscic para acento grave
+//acento grave `` template string
+
+// estilo o el prettier
+const soyUnString = 'hola "mundo"'
+console.log(soyUnString)
+
+//number
+const valor1 = 30 //si lo pones entre comillas ya no es un balor numero sino un texto asi "30"
+console.log(typeof valor1) //te dice si es un string que es texto o number si es numero.
+
+//booolen true o false
+const estaLloviendo =true
+console.log(estaLloviendo)
+
+//compuestos:
+
+//objetos y arreglos
+
+//Operadores: 
+
+//= + - / % (modulo) el residuo de una division
+console.log(10/2)
